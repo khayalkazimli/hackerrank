9112c46549a64ff1488a789c402e7b2cabcf40b3
khayalkazimli <82050458+khayalkazimli@users.noreply.github.com>
2021-10-27 20:33:35 +0400

    Update Result.java

--- src/Result.java
+++ src/Result.java
@@ -24,22 +24,4 @@ public class Result {
     }
 }
 
-class Solution {
-    public static void main(String[] args) throws IOException {
-
-        int n;
-        List<Integer> ar = new ArrayList<>();
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter number : ");
-        n = input.nextInt();
-        System.out.print("Enter values : ");
-        for (int i = 0; i < n; i++) {
-            ar.add(input.nextInt());
-        }
-
-        int result = Result.simpleArraySum(ar);
-        System.out.println(result);
-    }
-}
 
